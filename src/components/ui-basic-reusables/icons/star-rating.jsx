6f9bd87d22f78ade8f2c6de6683718155a43c612
Tara Troiano <troianot1@montclair.edu>
2025-05-15 17:33:16 -0400

    change to star rating

--- src/components/ui-basic-reusables/icons/star-rating.jsx
+++ src/components/ui-basic-reusables/icons/star-rating.jsx
@@ -1,15 +1,19 @@
 import "./star-rating.css";
 
 function StarRating({ rating }) {
+  const starCount = 5;
+  const letterSpacing = 0.24; // in rem
   const roundedRating = Math.round(rating * 10) / 10;
+  const overlayWidth = `calc(${(roundedRating / starCount) * 100}% - ${
+    (roundedRating - 1) * letterSpacing
+  }rem)`;
 
   return (
     <div className="star-rating">
-      <div className="star-background">★★★★★</div>
-      <div
-        className="star-overlay"
-        style={{ width: `${(roundedRating / 5) * 100}%` }}
-      >
+      <span className="star-layout">★★★★★</span> {/* LAYOUT CONTAINER */}
+      <div className="star-layer star-background-bottom">★★★★★</div>
+      <div className="star-layer star-background-top">★★★★★</div>
+      <div className="star-layer star-overlay" style={{ width: overlayWidth }}>
         ★★★★★
       </div>
     </div>
